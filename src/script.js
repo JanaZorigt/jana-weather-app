2d04c279b3ca06a8431594dbb5c6ffb90b1f0cd0
JanaZorigt <91855087+JanaZorigt@users.noreply.github.com>
2021-11-12 22:48:24 -0600

    Added relevant icon from API

--- src/script.js
+++ src/script.js
@@ -39,6 +39,16 @@ function displayWeatherCondition(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
